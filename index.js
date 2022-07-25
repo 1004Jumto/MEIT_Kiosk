af0ea67e8b81c1de1b8ad0cd09bd0cf5936550a4
YEJIN LEE <96861450+1004Jumto@users.noreply.github.com>
2022-07-26 04:24:36 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+let p = null;
+let texts;
+var keyword;
+
+function mic(){
+    window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+   let recognition = new SpeechRecognition();
+   recognition.interimResults = true;
+   recognition.lang = 'ko-KR';
+
+   let makeNewTextContent = function() {
+     p = document.createElement('p');
+     document.querySelector('.words').appendChild(p);
+   };
+
+   recognition.start();
+   recognition.onstart = function() {
+     makeNewTextContent();
+   };
+   recognition.onend = function() {
+     recognition.start();
+   };
+
+   recognition.onresult = function(e) {
+     texts = Array.from(e.results).map(results => results[0].transcript).join("");
+
+     texts.replace(/느낌표|강조|뿅/gi, '❗️');
+
+     keyword = JSON.stringify(texts);
+     console.log(keyword);
+
+     p.textContent = texts;
+   };
+}
+  
+function check(){
+    var flag = confirm(texts+"이/가 맞습니까?");
+    if(flag){
+     window.open("Key_.html", "_self");
+    }
+}
+console.log(keyword);
+
