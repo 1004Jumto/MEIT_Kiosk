519178c48e06ac41df9e6ca9e18cc550c054b2cb
YEJIN LEE <leeyjin0926@gmail.com>
2022-07-27 04:05:28 +0900

    2022-07-27-lap final static end

--- Key_Search.js
+++ Key_Search.js
@@ -1,5 +1,4 @@
-var keyword;
-
+var keyword = localStorage.getItem("text");
 
 /////////////////////////////////* json에서 메뉴 데이터 가져오기 *//////////////////////////////////////
 
@@ -10,7 +9,7 @@ var MenuData_Name = new Array();     //json에서 가져온 메뉴 이름 배열
 var MenuData_URL = new Array();      //json에서 가져온 메뉴 그림 배열
 var MenuData_Price = new Array();    //json에서 가져온 메뉴 가격 배열
 
-MenuData = JSON.parse(jsonTmp);         //JSON형태로 저장된 메뉴데이터를 자바스크립트로 다시 파싱
+MenuData = JSON.parse(jsonTmp);      //JSON형태로 저장된 메뉴데이터를 자바스크립트로 다시 파싱
 
 for(i=0; i<MenuData.length; i++){
     MenuData_Name[i] = MenuData[i].name;
@@ -24,8 +23,10 @@ function search(){
   
   var TmpObj;
   for(k=0; k < MenuData_Name.length; k++){
+
       TmpObj = new Object();
       var str = MenuData_Name[k];
+
       if(str.indexOf(keyword) >= 0){
           TmpObj.searched = str;
           TmpObj.num = k;
@@ -59,62 +60,7 @@ window.onload = function(){
         document.body.append(winDiv);    
    
     }
-
 }
 
-
-
-
-/*최장공통부분수열 알고리즘 */
-//Subsequence
-/*
-const string1 = string[0];
-const string2 = string[1];
-const s1 = string1.length;
-const s2 = string2.length;
-
-const dp = Array.from(new Array(s1 + 1), () => new Array(s2 + 1));
-for (let i = 0; i <= s1; i++) {
-  dp[i][0] = 0;
-}
-for (let j = 0; j <= s2; j++) {
-  dp[0][j] = 0;
-}
-
-for (let i = 1; i <= s1; i++) {
-  for (let j = 1; j <= s2; j++) {
-    if (string1.charAt(i - 1) === string2.charAt(j - 1)) {
-      dp[i][j] = dp[i - 1][j - 1] + 1;
-    } else {
-      dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-    }
-  }
-}
-console.log(dp[s1][s2]);
-*/
-
-//SubString
-/*
-let answer = 0;
-
-let LCS = new Array(s.length+1);
-for(let i=0; i<LCS.length; i++){
-  LCS[i] = new Array(t.length+1).fill(0);
-}
-
-for(let i=1; i<=s.length; i++){
-  for(let j=1; j<=t.length; j++){
-    if(s[i-1]===t[j-1])
-      LCS[i][j] = LCS[i-1][j-1] + 1;
-    else
-      LCS[i][j] = 0;
-
-    if(LCS[i][j] > answer)
-      answer = LCS[i][j];
-  }  
-}
-
-console.log(answer);
- */
 
 
--- index.js
+++ index.js
@@ -26,16 +26,17 @@ function mic(){
 
    recognition.onresult = function(e) {
      texts = Array.from(e.results).map(results => results[0].transcript).join("");
-
      texts.replace(/느낌표|강조|뿅/gi, '❗️');
   
      p.textContent = texts;
+     localStorage.setItem("text", texts);
    };
-  
 }
-  
+
 function check(){
     var flag = confirm(texts+" 이(가) 맞습니까?");
+    console.log(texts);
+
     if(flag){
      window.open("Key_.html", "_self");
     }
